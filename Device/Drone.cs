0a62ca41cfdcde4ae59a1179473bd4316bcc293d
nagykrisa <nagykrisa@gmail.com>
2017-11-02 16:18:38 +0100

    IoT HUBon Deviceok létrehozva, üzenet fogadása és küldése tesztelésre vár
    Két külön device, egy  Scout drone aki FlowerFieldeket küld üzenetben a hubnak és egy Pollinate drone aki FlowerField üzenetet vár és az ebből kiszedett lokációra megy gyűjteni. (+ idle dron semmittevő, ha később szeretnénk ilyet belerakni)
    Tesztelni : A Pollinate Dron-nak küldött FlowerField megérkezik-e? A Scout Drone üzenete elér-e a Hubhoz ?

--- Device/Drone.cs
+++ Device/Drone.cs
@@ -9,20 +9,24 @@ using RoBee.Utils;
 namespace RoBee {
 	namespace Device {
 		public class Drone {
-			public Drone(Location<double> pos, double speed, double range, DroneType type) {
+			public Drone(double sight,Location<double> pos, double speed, double range, DroneType type) {
 				typeOfDrone = type;
 				rangeOfDrone = range;
 				speedOfDrone = speed;
+                angleOfSight = sight;
 				locationOfDrone = pos;
 				locationToReach = new Location<double>();
+                fF = new FlowerField(null,0.0);
 			}
-			public Location<double> locationOfDrone;
-			public double speedOfDrone;
-			public double rangeOfDrone;
+            public FlowerField fF { get; set; }
+			public Location<double> locationOfDrone { get; set; }
+			public double speedOfDrone { get; set; }
+			public double rangeOfDrone { get; set; }
+            public double angleOfSight { get; set; }
+			public DroneType typeOfDrone { get; set; }
+			public Location<double> locationToReach { get; set; }
 
-			public DroneType typeOfDrone;
 
-			public Location<double> locationToReach;
 			public double simulateMovementTime() {
 				double distance = Math.Sqrt((locationToReach.X - locationOfDrone.X) * (locationToReach.X - locationOfDrone.X) + (locationToReach.Y - locationOfDrone.Y) * (locationToReach.Y - locationOfDrone.Y));
 				double time = distance / speedOfDrone;
@@ -30,6 +34,59 @@ namespace RoBee {
 				locationOfDrone.Y = locationToReach.Y;
 				return time;
 			}
+            public bool lookForFlowerField()
+            {
+                bool flowerFieldIsFound = false;
+                Random rand = new Random();
+                int findRate = 5;
+                if (findRate > rand.Next(100))
+                {
+                    flowerFieldIsFound = true;
+                    double flowerX = rand.NextDouble()*angleOfSight + locationOfDrone.X;
+                    double flowerY = rand.NextDouble()*angleOfSight + locationOfDrone.Y;
+
+                    Location<double> flowerFieldLocation = new Location<double>(flowerX, flowerY);
+                    double flowerFieldArea = rand.NextDouble() * 2;
+
+                    fF.Area = flowerFieldArea;
+                    fF.Loc = flowerFieldLocation;
+
+                }
+                    return flowerFieldIsFound;
+            }
+            public void moveRandom(double timeMilisec)
+            {
+                Random rand = new Random();
+                double distance = (timeMilisec / 1000) * speedOfDrone;
+                double moveOnX = Math.Sqrt( (distance * distance) - rand.Next(0, (int)(distance * distance)) );
+                double moveOnY = Math.Sqrt( (distance * distance) - (moveOnX * moveOnX) );
+                int upDown = rand.Next(4);
+                if( upDown == 0)
+                {
+                    locationOfDrone.X = locationOfDrone.X + moveOnX;
+                    locationOfDrone.Y = locationOfDrone.Y + moveOnY;
+                }
+                if(upDown == 1)
+                {
+                    locationOfDrone.X = locationOfDrone.X - moveOnX;
+                    locationOfDrone.Y = locationOfDrone.Y + moveOnY;
+                }
+                if (upDown == 2)
+                {
+                    locationOfDrone.X = locationOfDrone.X - moveOnX;
+                    locationOfDrone.Y = locationOfDrone.Y - moveOnY;
+                }
+                if (upDown == 3)
+                {
+                    locationOfDrone.X = locationOfDrone.X + moveOnX;
+                    locationOfDrone.Y = locationOfDrone.Y - moveOnY;
+                }
+            }
+            public void setDestinationToHive()
+            {
+                locationToReach.X = 0;
+                locationToReach.Y = 0;
+            }
 		}
 	}
 }
--- Device/Program.cs
+++ Device/Program.cs
@@ -11,49 +11,30 @@ using RoBee.Utils;
 namespace RoBee {
     namespace Device {
         public class MessageContent {
-
-            /// <summary>
-            /// allapotlekérdezéshez
-            /// @droneType
-            /// @deviceID
-            /// @currentPosition
-            /// 
-            /// kommunikaciohoz
-            /// @positionToReach
-            /// @positionOfFlower
-            /// </summary>
-
-            public Location<double> positionToReach;
-            public Location<double> positionOfFlower;
-
-            public string droneType;
+            public FlowerField positionOfFlowerField;
             public string deviceId;
-            public Location<double> currentPosition;
         }
 
         class Program {
             static DeviceClient deviceClient;
-            static string iotHubUri = "{IOT_HUB_URI}";
-            static string deviceKey = "{DEVICE_KEY}";
-            static string deviceId = "{DEVICE_ID}";
+            static string iotHubUri = "RoBee.azure-devices.net";
+            static string deviceKey = "E+UDcAQFQbTqXHCMxMX7kTFf1vSPynCzE4RPamf6JLA=";
+            static string deviceId = "PollinateDrone1";
            
             static void Main(string[] args) {
                 Location<double>startingLocation = new Location<double>();
                 startingLocation.X = 0;
                 startingLocation.Y = 0;
-
-                Drone drone = new Drone(startingLocation,2,50,DroneType.Idle);
-
-                //deviceClient =  DeviceClient.Create(iotHubUri, new DeviceAuthenticationWithRegistrySymmetricKey(deviceId, deviceKey));
-
-                RunDeviceAsync(drone);
+                Drone drone = new Drone(2,startingLocation,2,50,DroneType.Pollinate);
+                deviceClient =  DeviceClient.Create(iotHubUri, new DeviceAuthenticationWithRegistrySymmetricKey(deviceId, deviceKey));
+                RunDevice(drone);
                 ReceiveC2dAsync(drone);
                 Console.ReadLine();
             }
 
-            public static async void RunDeviceAsync(Drone d)
-            {   
-                
+            public static void RunDevice(Drone d)
+            {
+
                 if (DroneType.Scout.Equals(d.typeOfDrone))
                 {
                     RunScoutingDrone(d);
@@ -67,97 +48,98 @@ namespace RoBee {
                     RunIdleDrone(d);
                 }
             }
-           
-            //Todo üzenetek feldolgozása és átadása a drónnak
+            
             private static async void ReceiveC2dAsync(Drone d)
             {
-                Console.WriteLine("\nÜzenetek fogadása az IoT Hub-tól");
+                Console.WriteLine("\nUzenetek fogadasa az IoT Hub-tol");
                 while (true)
                 {
                     Message receivedMessage = await deviceClient.ReceiveAsync();
                     if (receivedMessage == null) continue;
-
-
                     string data = Encoding.ASCII.GetString(receivedMessage.GetBytes());
-                    ///Itt felkéne okosan dolgozni ezt az üzenetet hogy kivehető legyen a virág pozíciója
-                    ///vagy a felderítendő irány
-                    ///beallit d.DroneType = kapott érték
-                    ///beallit d.LocationToReach = kapott érték
-
-
+                    FlowerField toReach = JsonConvert.DeserializeObject<FlowerField>(data);
+                    d.locationToReach = toReach.Loc;
+   
                     Console.ForegroundColor = ConsoleColor.DarkRed;
-                    Console.WriteLine("Üzenet érkezett: {0}", Encoding.ASCII.GetString(receivedMessage.GetBytes()));
+                    Console.WriteLine("Uzenet erkezett: {0}", Encoding.ASCII.GetString(receivedMessage.GetBytes()));
                     Console.ResetColor();
                     await deviceClient.CompleteAsync(receivedMessage);
                 }
             }
-            //TODO
+            
             private static async void RunScoutingDrone(Drone d)
             {
+                /// 1.Random generált útvonal mentén feledríti a virágokat.
+                /// 2.Ha virágot talált elküldi üzenetben a koordinátáit a kaptárba (1sec).
+                /// 3.Folytatja a felderítést.
                 Console.WriteLine("Szimulalt Felderito Dron\n");
                 while (DroneType.Scout.Equals(d.typeOfDrone))
                 {
-
+                   Console.WriteLine("{0} Viragmezot keresek", DateTime.Now);
+                    bool flowerFound = d.lookForFlowerField();
+                   if (flowerFound) {
+                     var messageContent = new MessageContent();
+                        messageContent.deviceId = deviceId;
+                        messageContent.positionOfFlowerField = d.fF;
+                        var messageString = JsonConvert.SerializeObject(messageContent);
+                        var message = new Message(Encoding.ASCII.GetBytes(messageString));
+                  
+                        await deviceClient.SendEventAsync(message);
+                        Console.WriteLine("{0} Viragmezot talaltam!", DateTime.Now);
+                        Console.WriteLine("{0} > Uzenet kuldese: {1}", DateTime.Now, messageString);
+                   }
+                   flowerFound = false;
+                   await Task.Delay(2000);
+                   d.moveRandom(2000);
+                   Console.WriteLine("{0} Nem talaltam mezot ezert tovabb kerestem x:{1} y:{2}", DateTime.Now, d.locationOfDrone.X,d.locationOfDrone.Y);
                 }
-                /// 1.Random generált vagy paraméterben kapott útvonal mentén feledríti a virágokat.
-                /// 2.Ha virágot talált elküldi üzenetben a koordinátáit a kaptárba (1sec).
-                /// 3.Folytatja a felderítést, szükség esetén feltölti magát.
-
             }
-            //TODO
+            
             private static async void RunPollinateDrone(Drone d)
             {
-                Console.WriteLine("Szimulált Beporzo Dron\n");
-                while (DroneType.Pollinate.Equals(d.typeOfDrone))
-                {
-
-                }
                 /// 1.Paraméterben átadott virághoz indul koordinata alapján.
                 /// 2.Ha oda ért vár 2sec-et (beporzás) és üzenetet küld  hogy megtörtént a beporzás.
                 /// 3.Vissza indul a kaptárba.
+                Console.WriteLine("Szimulált Beporzo Dron\n");
+                if(DroneType.Pollinate.Equals(d.typeOfDrone))
+                {
+                    double movementTime= d.simulateMovementTime();
+                    Console.WriteLine("Indulok a viraghoz, {0} masodperc\n", movementTime);
+                    await Task.Delay((int)movementTime * 1000);
+                    Console.WriteLine("Beporzok 2 masodperc\n");
+                    await Task.Delay(2000);
+                    Console.WriteLine("Vissza indulok a kaptarba \n");
+                    d.setDestinationToHive();
+                    movementTime = d.simulateMovementTime();
+                    await Task.Delay((int)movementTime * 1000);
+                    Console.WriteLine("A vissza indulok a kaptarba {0} masodperc mulva oda erek \n", movementTime);
+                    Console.WriteLine("A kaptarba erkeztem toltodok\n");
+                }
+                
             }
-            //kész
+            
             private static async void RunIdleDrone(Drone d)
             {
                 /// 1.Ha a kaptárban (0;0) van, és nincs feladata akkor tölti magát .
                 /// 2.Ha nincs a kaptárban akkor elindul az kaptár felé és tölt amíg feladatot nem kap.
                 Console.WriteLine("Szimulált Piheno Dron\n");
 
-                d.locationToReach.X = 0;
-                d.locationToReach.Y = 0;
-                
+                d.setDestinationToHive();
+               
                     if ((d.locationToReach.X == d.locationOfDrone.X) && (d.locationToReach.Y == d.locationOfDrone.Y))
                     {
                         Console.WriteLine("A kaptarban voltam es toltodok\n");
                     }
+
                     if((d.locationToReach.X != d.locationOfDrone.X) || (d.locationToReach.Y != d.locationOfDrone.Y))
                     {
-                        double waitTime = d.simulateMovementTime();
-                        Console.WriteLine("A vissza indulok a kaptarba {0} masodperc mulva oda erek \n",waitTime);
-                        await Task.Delay((int)waitTime * 1000);
+                        double movementTime = d.simulateMovementTime();
+                        Console.WriteLine("A vissza indulok a kaptarba {0} masodperc mulva oda erek \n",movementTime);
+                        await Task.Delay((int)movementTime * 1000);
                          Console.WriteLine("A kaptarba erkeztem toltodok\n");
                     }
             }
 
-            //private static async void SendDeviceToCloudMessagesAsync() {
-            //    Random rand = new Random();
-
-            //    while (true) {
-            //        double currentWindSpeed = 10 + rand.NextDouble() * 40;
-
-            //        var messageContent = new MessageContent();
-            //        messageContent.deviceId = deviceId;
-            //        messageContent.windSpeed = currentWindSpeed;
-
-            //        var messageString = JsonConvert.SerializeObject(messageContent);
-            //        var message = new Message(Encoding.ASCII.GetBytes(messageString));
-
-            //        await deviceClient.SendEventAsync(message);
-            //        Console.WriteLine("{0} > Üzenet küldése: {1}", DateTime.Now, messageString);
-
-            //        await Task.Delay(1000);
-            //    }
-            //}
         }
     }
 }
